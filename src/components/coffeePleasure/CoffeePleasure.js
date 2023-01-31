4da11e4c29458b5c5fd880b286cd174cfe74c1a0
Danil <danil.miroshnichenko2022@gmail.com>
2023-01-31 20:18:57 +0200

    Bg fix

--- src/components/coffeePleasure/CoffeePleasure.js
+++ src/components/coffeePleasure/CoffeePleasure.js
@@ -1,8 +1,10 @@
 import CoffeeNavigation from "../coffeeNavigation/CoffeeNavigation"
 
+import './coffeePleasure.scss'
+
 const CoffeePleasure = () => {
     return (
-        <div className="header-our header">
+        <div className="header-our header header-goods">
             <div className="container">
                 <CoffeeNavigation/>
                 <h1 className="header-title header-our-title">For your pleasure</h1>
